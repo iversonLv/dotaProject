ea94ab6089122abdfa939f402b551fc352e32e16
iversonLv <iverson_3_3_3@126.com>
2021-02-26 18:07:41 +0800

    build update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   BASE_API_URL: 'https://api.opendota.com/api',
-  BASE_LOCAL_DATA_URL: '/assets/data',
-  BASE_GIT_RAW_DATA_URL: '/assets/data' || 'https://raw.githubusercontent.com/odota/dotaconstants/master/build'
+  BASE_LOCAL_DATA_URL: '/dotaProject/assets/data',
+  BASE_GIT_RAW_DATA_URL: '/dotaProject/assets/data' || 'https://raw.githubusercontent.com/odota/dotaconstants/master/build'
 };
