01c9774d2201334f715dce167b84c40a2cffb016
iversonLv <iverson_3_3_3@126.com>
2021-03-02 23:18:10 +0800

    quick fix rank tier tooltip and permil for heroes pubilc table

--- src/app/shared/utils/rank-tier.pipe.ts
+++ src/app/shared/utils/rank-tier.pipe.ts
@@ -14,7 +14,11 @@ export class RankTierPipe implements PipeTransform {
         // If rank tier is Immortal won't have star num
         return `${this.tierMapping[tier]}`;
       } else {
-        return `${this.tierMapping[tier]} [ ${star} ]`;
+        if (star === undefined) {
+          return `${this.tierMapping[tier]}`;
+        } else {
+          return `${this.tierMapping[tier]} [ ${star} ]`;
+        }
       }
     } else {
       return `Unknown`;
