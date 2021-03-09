1e5647cd994022f96258bc122ac36d031648a4de
iversonLv <iverson_3_3_3@126.com>
2021-03-09 23:50:06 +0800

    Create players ward map page, Update README.MD

--- src/app/players/pages/wardmap/wardmap.component.ts
+++ src/app/players/pages/wardmap/wardmap.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// ngrx
+import { Store } from '@ngrx/store';
+import * as playersAction from '../../store/players.actions';
 
 @Component({
   selector: 'app-wardmap',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wardmap.component.scss']
 })
 export class WardmapComponent implements OnInit {
-
-  constructor() { }
+  accountId;
+  isLoading: false;
+  queryParams;
+  showLarge = false;
+  selectedMap;
+  wardMaps: any = [];
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private store: Store<{ playersWardMaps: any }>
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParamMap.subscribe(data => this.queryParams = data);
+    this.accountId = this.router.url.split('/')[2];
+
+    this.store.dispatch(new playersAction.LoadPlayersWardMaps(this.accountId, this.queryParams));
+    this.store.select('playersWardMaps').subscribe(data => {
+      this.isLoading = data.isLoading;
+      if (!data.isLoading) {
+        const warmMpasData = {...data.wardMaps};
+        this.wardMaps.push({ ...this.calSeriesData(warmMpasData.obs, 900), title: 'Observer'});
+        this.wardMaps.push({ ...this.calSeriesData(warmMpasData.sen, 900), title: 'Sentry'});
+        this.isLoading = data.isLoading;
+        console.log(this.wardMaps);
+        return this.wardMaps;
+      }
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  showLargeMap(map): any {
+    // if (this.selectedMap === map) {
+    //   this.selectedMap = null;
+    // } else {
+    //   this.selectedMap = map;
+    // }
   }
 
   // if map is 900*900, data should be *7
@@ -35,23 +74,28 @@ export class WardmapComponent implements OnInit {
   * }]
   */
  // use this format https://github.com/odota/web/blob/d94cc9693ad866a1d833876623de839e01b49531/src/utility/index.jsx#L311
-  unpackPositionData(input, mapSize): any[] {
-  if (typeof input === 'object' && !Array.isArray(input)) {
-    const result = [];
+ calSeriesData(data, mapSize): any {
+    if (typeof data === 'object' && !Array.isArray(data)) {
+      const value = [];
+      const result = {
+        data: [],
+        min: null,
+        max: null
+      };
 
-    Object.keys(input).forEach((x) => {
-      Object.keys(input[x]).forEach((y) => {
-        result.push({
-          x: (Number(x) - 64) * mapSize / 128,
-          y: (128 - (Number(y) - 64)) * mapSize / 128,
-          value: input[x][y],
+      Object.keys(data).forEach((x) => {
+        Object.keys(data[x]).forEach((y) => {
+          value.push(data[x][y]);
+          result.data.push([
+            (Number(x) - 64) * mapSize / 128,
+            (128 - (Number(y) - 64)) * mapSize / 128,
+            data[x][y],
+          ]);
         });
       });
-    });
-
-    return result;
+      result.min = Math.min(...value);
+      result.max = Math.max(...value);
+      return result;
+    }
   }
-
-  return input;
-}
 }
