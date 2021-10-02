d5f9f5662d2f84de5daa73ef914f37bb5012576e
iversonLv <iverson_3_3_3@126.com>
2021-10-02 18:09:07 +0800

    Added ordinal num pipe another method

--- src/app/shared/utils/ordinal-num.pipe.ts
+++ src/app/shared/utils/ordinal-num.pipe.ts
@@ -30,3 +30,11 @@ export class OrdinalNumPipe implements PipeTransform {
   }
 
 }
+
+
+// const getOrdinal = (n) => {
+//   // TODO localize
+//   const s = ['th', 'st', 'nd', 'rd'];
+//   const v = n % 100;
+//   return n + (s[(v - 20) % 10] || s[v] || s[0]);
+// };
