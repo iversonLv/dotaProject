d2b799b1c0d2c83f3ff8c0a76a8e1ec875141e01
iversonLv <iverson_3_3_3@126.com>
2021-10-03 23:09:09 +0800

    Update tables with paginatior that show ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: 'null'. Current value: '0'.

--- src/app/heros/components/table-heros-matches/table-heros-matches.component.ts
+++ src/app/heros/components/table-heros-matches/table-heros-matches.component.ts
@@ -32,8 +32,10 @@ export class TableHerosMatchesComponent implements OnInit {
   // material table bottom paginator
   // officaly, material table only support one paginator
   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
-    this.paginator = mp;
-    this.setDataSourceAttributes();
+    setTimeout(() => {
+      this.paginator = mp;
+      this.setDataSourceAttributes();
+    });
   }
   paginator;
   sort;
--- src/app/heros/components/table-matchups-players/table-matchups-players.component.ts
+++ src/app/heros/components/table-matchups-players/table-matchups-players.component.ts
@@ -35,8 +35,10 @@ export class TableMatchupsPlayersComponent implements OnInit {
   // material table bottom paginator
   // officaly, material table only support one paginator
   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
-    this.paginator = mp;
-    this.setDataSourceAttributes();
+    setTimeout(() => {
+      this.paginator = mp;
+      this.setDataSourceAttributes();
+    });
   }
 
   sort;
