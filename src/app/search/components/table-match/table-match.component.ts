87fdba326401a1350443d27999bc003ed354143c
iversonLv <iverson_3_3_3@126.com>
2021-10-04 14:26:29 +0800

    Update table-match of serch page for heroes with dotaconstatns and remove exisiting heroesLocal service

--- src/app/search/components/table-match/table-match.component.ts
+++ src/app/search/components/table-match/table-match.component.ts
@@ -10,11 +10,13 @@ import { Store } from '@ngrx/store';
 // model
 import { ISingleMatchData } from 'src/app/matches/model/onematch';
 import * as matchesActions from 'src/app/matches/store/matches.actions';
+import { IheroLocal } from 'src/app/heros/model/heroLocal';
 
 // service
-import { HerosService } from 'src/app/heros/services/heros.service';
 import { SkillService } from 'src/app/services/skill.service';
-import { IheroLocal } from 'src/app/heros/model/heroLocal';
+
+// dotaconstants
+import heroes from 'dotaconstants/build/heroes.json';
 
 @Component({
   selector: 'app-table-match',
@@ -29,12 +31,11 @@ export class TableMatchComponent implements OnInit {
   dataSource = new MatTableDataSource();
 
   // User for hero modal to mapping
-  heroesLocal: IheroLocal;
+  heroes: any = heroes;
   skillLocal: any;
 
   constructor(
     private router: Router,
-    private herosService: HerosService,
     private skillService: SkillService,
     private activatedRoute: ActivatedRoute,
     private store: Store<{ singleMatch: ISingleMatchData }>
@@ -59,17 +60,7 @@ export class TableMatchComponent implements OnInit {
       console.log(err);
     });
 
-    // get all heroes local data
-    this.getHeroesLocal();
     this.getSkillLocal();
-  }
-
-  getHeroesLocal(): any {
-    this.herosService.getHeroesLocal().subscribe(data => {
-      this.heroesLocal = data;
-    }, err => {
-      console.log(err);
-    });
   }
 
   getSkillLocal(): any {
