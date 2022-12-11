fc4fa6b07a77170a9f8721b972088657da81851d
iversonLv <iverson_3_3_3@126.com>
2022-12-11 15:23:22 +0800

    Update name destruct pipe with toSeparator

--- src/app/shared/utils/name-destruct.pipe.ts
+++ src/app/shared/utils/name-destruct.pipe.ts
@@ -5,13 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NameDestructPipe implements PipeTransform {
 
-  transform(value: string, separator: string, separatorPlacement: number, upperCase?: string): unknown {
+  transform(value: string, fromSeparator: string, fromSeparatorPlacement: number, upperCase?: string, toSeparator?: string): unknown {
     // example lobby_type name is lobby_type_normal, we need destruct only last string and uppercase
-    if (value !== null && value.split(separator).length > 1) {
+    if (value !== null && value.split(fromSeparator).length > 1) {
       if (upperCase === 'upperCase') {
-        return value.split(separator).splice(separatorPlacement).map(i => i.charAt(0).toUpperCase() + i.slice(1, i.length)).join(' ');
+        // tslint:disable-next-line:max-line-length
+        return value.split(fromSeparator).splice(fromSeparatorPlacement).map(i => i.charAt(0).toUpperCase() + i.slice(1, i.length)).join(toSeparator ?? ' ');
       } else {
-        return value.split(separator).splice(separatorPlacement).join(' ');
+        return value.split(fromSeparator).splice(fromSeparatorPlacement).join(toSeparator ?? ' ');
       }
     } else {
       return value;
