6894044a402f229701522cc44bbfbc5ab7d7dc7a
iversonLv <iverson_3_3_3@126.com>
2024-04-11 14:41:48 +0800

    Remove some useless cols and fixed color

--- src/app/matches/components/table-match-detail-benchmarks/table-match-detail-benchmarks.component.ts
+++ src/app/matches/components/table-match-detail-benchmarks/table-match-detail-benchmarks.component.ts
@@ -7,7 +7,7 @@ import { IheroLocal } from 'src/app/heros/model/heroLocal';
 @Component({
   selector: 'app-table-match-detail-benchmarks',
   templateUrl: './table-match-detail-benchmarks.component.html',
-  styleUrls: ['./table-match-detail-benchmarks.component.scss']
+  styleUrls: ['./table-match-detail-benchmarks.component.scss'],
 })
 export class TableMatchDetailBenchmarksComponent implements OnInit {
   @Input() data: any;
@@ -24,39 +24,31 @@ export class TableMatchDetailBenchmarksComponent implements OnInit {
   herosBenchmarksResultKey: any = {
     gold_per_min: {
       tooltip: 'GOLD FARMED PER MINUTE',
-      title: 'GPM'
+      title: 'GPM',
     },
     xp_per_min: {
       tooltip: 'EXPERIENCE GAINED PER MINUTE',
-      title: 'XPM'
+      title: 'XPM',
     },
     hero_damage_per_min: {
       tooltip: 'HERO DAMAGE PER MINUTE',
-      title: 'HDM'
+      title: 'HDM',
     },
     hero_healing_per_min: {
       tooltip: 'HERO HEALING PER MINUTE',
-      title: 'HHM'
+      title: 'HHM',
     },
     kills_per_min: {
       tooltip: 'KILLS PER MINUTE',
-      title: 'KPM'
+      title: 'KPM',
     },
     last_hits_per_min: {
       tooltip: 'LAST HITS PER MINUTE',
-      title: 'LHM'
-    },
-    lhten: {
-      tooltip: 'LAST HITS AT 10 MINUTES',
-      title: 'LH@10'
-    },
-    stuns_per_min: {
-      tooltip: 'SECONDS OF HERO STUNS PER MINUTE',
-      title: 'SPM'
+      title: 'LHM',
     },
     tower_damage: {
       tooltip: 'AMOUNT OF DAMAGE DEALT TO TOWERS',
-      title: 'TD'
+      title: 'TD',
     },
   };
 
@@ -70,10 +62,8 @@ export class TableMatchDetailBenchmarksComponent implements OnInit {
     'hero_damage_per_min',
     'hero_healing_per_min',
     'tower_damage',
-    'stuns_per_min',
-    'lhten'
   ];
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     // this.extractData(this.data);
@@ -83,8 +73,18 @@ export class TableMatchDetailBenchmarksComponent implements OnInit {
   // extract matches players[] to less data to meet for this page table
   extractData(data): any[] {
     const finalData = [];
-    data.forEach(z => {
-      const { hero_id, randomed, player_slot, pred_vict, account_id, rank_tier, name, personaname, benchmarks} = z;
+    data.forEach((z) => {
+      const {
+        hero_id,
+        randomed,
+        player_slot,
+        pred_vict,
+        account_id,
+        rank_tier,
+        name,
+        personaname,
+        benchmarks,
+      } = z;
       finalData.push({
         hero_id,
         account_id,
@@ -95,7 +95,7 @@ export class TableMatchDetailBenchmarksComponent implements OnInit {
         pred_vict,
         randomed,
         // above is common data for player
-        benchmarks
+        benchmarks,
       });
     });
     return finalData;
@@ -116,7 +116,7 @@ export class TableMatchDetailBenchmarksComponent implements OnInit {
       return 'benchmarks-yellow';
     } else if (pct >= 0.6 && pct < 0.8) {
       return 'benchmarks-blue';
-    } else if (pct >= 8) {
+    } else {
       return 'benchmarks-green';
     }
   }
