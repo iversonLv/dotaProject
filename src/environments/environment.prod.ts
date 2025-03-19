d3b6fa59ebe906a42cfb95c61352c90c7c7dd038
RUMI LU <iverson_3_3_3@126.com>
2025-03-19 07:11:30 +0000

    Updated DC and environment version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  currentVersion: '7.37d',
+  currentVersion: '7.38b',
   currentMap:
     'https://www.opendota.com/assets/images/dota2/map/detailed_733.jpg',
   BASE_API_URL: 'https://api.opendota.com/api',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  currentVersion: '7.37d',
+  currentVersion: '7.38b',
   currentMap:
     'https://www.opendota.com/assets/images/dota2/map/detailed_733.jpg',
   BASE_API_URL: 'https://api.opendota.com/api',
